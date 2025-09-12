5c263fb5c44b3111d5ba740992f775b36a2c4d89
Abdulbosit <umarov@MacBook-Air-Abdulbosit.local>
2025-09-12 17:43:03 +0500

    sad

--- src/pages/Product-detail/index.tsx
+++ src/pages/Product-detail/index.tsx
@@ -4,7 +4,7 @@ import Newsletter from "../../components/newsletter/Newsletter";
 const ProductDetails = () => {
   return (
     <div>
-      ProductDetails
+      ProductDetails-ABdulbosit
       <Newsletter />
     </div>
   );
