2812c3cdc0211c3f960fa4a036881de780170d6c
elbekdamirov <elbekmain@gmail.com>
2025-09-13 22:04:47 +0500

    swiper + furniture

--- src/pages/home/components/Carusel.tsx
+++ src/pages/home/components/Carusel.tsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import swiper from "../../../assets/swiper.png";
+
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/swiper-bundle.css";
 
 const Carusel = () => {
-  return <div>Carusel - ELBEK</div>;
+  return (
+    <section>
+      <div className="pb-10">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper mb-8"
+        >
+          <SwiperSlide>
+            <img className="max-w-full" src={swiper} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="max-w-full" src={swiper} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="max-w-full" src={swiper} alt="" />
+          </SwiperSlide>
+        </Swiper>
+        <div className="flex items-center justify-between">
+          <div>
+            <h1 className="text-7xl font-poppins tracking-[-2px]">
+              Simply Unique<span className="text-gray-500">/</span>
+            </h1>
+            <h1 className="text-7xl font-poppins tracking-[-2px]">
+              Simply Better<span className="text-gray-500">.</span>
+            </h1>
+          </div>
+          <div className="w-[40%] pr-7">
+            <span className="font-inter text-[#6C7275] text-[16px]">
+              <b className="text-[#343839]">3legant</b> is a gift & decorations
+              store based in HCMC, Vietnam. Est since 2019.
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default React.memo(Carusel);
--- src/pages/home/components/Funiture.tsx
+++ src/pages/home/components/Funiture.tsx
@@ -1,7 +1,70 @@
 import React from "react";
+import { GoArrowRight } from "react-icons/go";
+import furniture from "../../../assets/furniture.png";
+import chair from "../../../assets/chair.png";
+import toaster from "../../../assets/toaster.png";
 
 const Funiture = () => {
-  return <div>Funiture - Elbek</div>;
+  return (
+    <section>
+      <div className="grid grid-cols-2 gap-6">
+        <div
+          className="row-span-2 h-[664px] p-12"
+          style={{ backgroundImage: `url(${chair})` }}
+        >
+          <div>
+            <h3 className="text-3xl font-poppins tracking-[-0.6px] mb-3">
+              Living Room
+            </h3>
+            <span className="inline-block">
+              <a
+                href="#"
+                className="relative flex items-center gap-2 font-inter tracking-[-0.4px] text-black pb-1 after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black after:w-0 hover:after:w-full after:transition-all after:duration-300"
+              >
+                Shop Now <GoArrowRight />
+              </a>
+            </span>
+          </div>
+        </div>
+        <div
+          className="px-8 py-10 bg-cover bg-center flex flex-col justify-end"
+          style={{ backgroundImage: `url(${furniture})` }}
+        >
+          <div>
+            <h3 className="text-3xl font-poppins tracking-[-0.6px] mb-3">
+              Bedroom
+            </h3>
+            <span className="inline-block">
+              <a
+                href="#"
+                className="relative flex items-center gap-2 font-inter tracking-[-0.4px] text-black pb-1 after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black after:w-0 hover:after:w-full after:transition-all after:duration-300"
+              >
+                Shop Now <GoArrowRight />
+              </a>
+            </span>
+          </div>
+        </div>
+        <div
+          className="px-8 py-10 bg-cover bg-center flex flex-col justify-end"
+          style={{ backgroundImage: `url(${toaster})` }}
+        >
+          <div>
+            <h3 className="text-3xl font-poppins tracking-[-0.6px] mb-3">
+              Kitchen
+            </h3>
+            <span className="inline-block">
+              <a
+                href="#"
+                className="relative flex items-center gap-2 font-inter tracking-[-0.4px] text-black pb-1 after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:bg-black after:w-0 hover:after:w-full after:transition-all after:duration-300"
+              >
+                Shop Now <GoArrowRight />
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default React.memo(Funiture);
