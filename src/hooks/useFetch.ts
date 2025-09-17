7afa75527788c5a6e5588bff302a2505f96bbd25
Ozodbekn <ozodbekjalilov07@gmail.com>
2025-09-17 11:02:53 +0500

    ozodbek 5.0

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
-import { api } from "../api";
+  import { useEffect, useState } from "react";
+  import { api } from "../api";
 
-interface IParams {
-    limit: number
-    skip?: number
-    sortBy?: string
-    order?:string
-}
+  interface IParams {
+      limit: number
+      skip?: number
+      sortBy?: string
+      order?:string
+  }
 
-export const useFetch = (entpoint: string, params?: IParams) => {
-  const [data, setData] = useState<any>(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  export const useFetch = (entpoint: string, params?: IParams) => {
+    const [data, setData] = useState<any>(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setLoading(true)
-    api
-        .get(entpoint, {params})
-        .then((res) => setData(res.data))
-        .catch(err => setError(err))
-        .finally(() => setLoading(false))
-  }, [entpoint, JSON.stringify(params)]);
+    useEffect(() => {
+      setLoading(true)
+      api
+          .get(entpoint, {params})
+          .then((res) => setData(res.data))
+          .catch(err => setError(err))
+          .finally(() => setLoading(false))
+    }, [entpoint, JSON.stringify(params)]);
 
-  return {data, error, loading}
-};
+    return {data, error, loading}
+  };
